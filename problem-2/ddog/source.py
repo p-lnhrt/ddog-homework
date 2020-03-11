bb7db2b0279dc0cb59fdacb8076c2b0088a42eb7
p-lnhrt <pierre.lienhart@gmail.com>
2020-03-11 19:11:45 +0100

    Add file downloader object BaseballFilesDownloader

--- problem-2/ddog/source.py
+++ problem-2/ddog/source.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import shutil
+import urllib
 
 
 class TempDir:
@@ -18,3 +19,27 @@ class TempDir:
     def __exit__(self, exc_type, exc_value, traceback):
         if self.remove:
             shutil.rmtree(self.path, ignore_errors=True)
+
+
+class BaseballFilesDownloader:
+    def __init__(self, tmp_dir_path, config):
+        self.tmp_dir_path = tmp_dir_path
+        self.formatted_tmp_file_name = config['DEFAULT']['TmpFileFormattedName']
+        self.formatted_url = config['DEFAULT']['FormattedSourceURL']
+
+    def download(self, years):
+        for year in years:
+            local_file_name = self.formatted_tmp_file_name.format(year=year)
+            url = self.formatted_url.format(year=year)
+            try:
+                with urllib.request.urlopen(url=url) as response:
+                    file_name = os.path.join(self.tmp_dir_path, local_file_name)
+                    with open(file_name, 'wb') as file_obj:
+                        file_obj.write(response.read())
+                logging.info('Successfully downloaded file for year {year:} at {path:}'
+                             .format(year=year, path=file_name))
+
+            except urllib.error.HTTPError as error:
+                logging.warning('Requesting URL "{url:}" failed returning the following HTTP error: '
+                                'Code: {code:} - {msg:}'.format(url=url, code=error.code, msg=error.msg))
+                continue
--- problem-2/main.py
+++ problem-2/main.py
@@ -1,5 +1,7 @@
 import configparser
 import logging
+import os
+import re
 import sys
 
 import ddog.cli
@@ -16,6 +18,14 @@ if __name__ == '__main__':
     parser = ddog.cli.CliArgParser(config=config)
     args = parser.parse_args(args=sys.argv[1:])
 
+    requested_year_range = range(args['from'], args['to'] + 1)
+    tmp_file_regex = config['DEFAULT']['TmpFileRegex']
+    regex = re.compile(tmp_file_regex)
     with ddog.source.TempDir(path=args['tmp'], remove=args['remove']) as tmp_dir_path:
-        pass
+        available_years = [int(regex.search(file_name).group(1)) for file_name in os.listdir(tmp_dir_path)
+                           if regex.match(file_name)]
+        missing_years = set(requested_year_range).difference(available_years)
+
+        files_downloader = ddog.source.BaseballFilesDownloader(tmp_dir_path=tmp_dir_path, config=config)
+        files_downloader.download(years=missing_years)
 
