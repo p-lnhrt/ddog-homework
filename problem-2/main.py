144d8ade7bbaac91e07becdc9a900e9695f86af5
p-lnhrt <pierre.lienhart@gmail.com>
2020-03-11 18:10:13 +0100

    Add logging

--- problem-2/main.py
+++ problem-2/main.py
@@ -1,4 +1,5 @@
 import configparser
+import logging
 import sys
 
 import ddog.cli
@@ -8,5 +9,9 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
 
+    level = config['DEFAULT']['LoggingLevel']
+    logging.basicConfig(level=level, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
+
     parser = ddog.cli.CliArgParser(config=config)
     args = parser.parse_args(args=sys.argv[1:])
+
