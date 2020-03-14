5bb06ef69d50bc2b54048df5d38672c477cef570
p-lnhrt <pierre.lienhart@gmail.com>
2020-03-14 18:24:25 +0100

    Add docstrings to ddog.source unit tests

--- problem-2/ddog/tests/test_source.py
+++ problem-2/ddog/tests/test_source.py
@@ -7,10 +7,16 @@ import ddog.source
 @mock.patch('logging.info')
 @mock.patch('os.mkdir')
 def test_tmp_dir_enter_non_existing_dir(mock_mkdir, mock_info):
+    """
+    Given a `ddog.source.TempDir` context manager associated with a non-yet existing local directory,
+    When I call the `__enter__` method of the context manager,
+    Then the `os.mkdir` function should be called once with the directory path and the `logging.info` function should be
+    called once.
+    """
     path = '/path/to/tmp'
-    remove = True
-    tmp_dir_context_manager = ddog.source.TempDir(path=path, remove=remove)
-    tmp_dir_context_manager.__enter__()
+    mock_cm = mock.Mock()
+    mock_cm.path = path
+    ddog.source.TempDir.__enter__(mock_cm)
     mock_mkdir.assert_called_once_with(path)
     mock_info.assert_called_once()
 
@@ -18,17 +24,27 @@ def test_tmp_dir_enter_non_existing_dir(mock_mkdir, mock_info):
 @mock.patch('logging.info')
 @mock.patch('os.mkdir')
 def test_tmp_dir_enter_existing_dir(mock_mkdir, mock_info):
+    """
+    Given a `ddog.source.TempDir` context manager associated with an already-existing local directory,
+    When I call the `__enter__` method of the context manager,
+    Then the the `logging.info` function should be called once.
+    """
     path = '/path/to/tmp'
-    remove = True
+    mock_cm = mock.Mock()
+    mock_cm.path = path
     mock_mkdir.side_effect = FileExistsError
-    tmp_dir_context_manager = ddog.source.TempDir(path=path, remove=remove)
-    tmp_dir_context_manager.__enter__()
+    ddog.source.TempDir.__enter__(mock_cm)
     mock_info.assert_called_once()
 
 
 @mock.patch('logging.info')
 @mock.patch('shutil.rmtree')
 def test_tmp_dir_exit_rm_true(mock_rmtree, mock_info):
+    """
+    Given a `ddog.source.TempDir` context manager with a `remove` attribute set to `True`,
+    When I call the `__exit__` method of the context manager,
+    Then the `shutil.rmtree` and `logging.info` functions should be called once.
+    """
     path = '/path/to/tmp'
     remove = True
     tmp_dir_context_manager = ddog.source.TempDir(path=path, remove=remove)
@@ -40,6 +56,11 @@ def test_tmp_dir_exit_rm_true(mock_rmtree, mock_info):
 @mock.patch('logging.info')
 @mock.patch('shutil.rmtree')
 def test_tmp_dir_exit_rm_false(mock_rmtree, mock_info):
+    """
+    Given a `ddog.source.TempDir` context manager with a `remove` attribute set to `False`,
+    When I call the `__exit__` method of the context manager,
+    Then the `shutil.rmtree` and `logging.info` functions should not be called.
+    """
     path = '/path/to/tmp'
     remove = False
     tmp_dir_context_manager = ddog.source.TempDir(path=path, remove=remove)
@@ -50,6 +71,12 @@ def test_tmp_dir_exit_rm_false(mock_rmtree, mock_info):
 
 @mock.patch('os.listdir')
 def test_get_missing_years(mock_listdir):
+    """
+    Given a `ddog.source.BaseballFilesLoader` object set with a year range going from 2000 to 2010 (included) and a
+    temporary directory containing baseball-statistics files from year 2000 to 2007 included,
+    When I call its `_get_missing_years` method,
+    Then I should be returned a set containing the years missing in the temporary directory (2008, 2009 and 2010).
+    """
     min_year = 2000
     mock_loader = mock.Mock(min_year=min_year,
                             max_year=2010,
