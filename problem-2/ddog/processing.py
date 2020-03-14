f25c9645f3b5c8b9d99bb22f975110919919c48a
p-lnhrt <pierre.lienhart@gmail.com>
2020-03-14 13:53:17 +0100

    Add docstrings to ddog.compute

--- problem-2/ddog/processing.py
+++ problem-2/ddog/processing.py
@@ -1,13 +1,37 @@
+"""
+This modules gathers all the classes and functions dedicated to the processing of metrics on baseball data.
+"""
 import itertools
 
 import numpy as np
 
 
 class TripleCounter:
+    """ This class encapsulates the logic dedicated tho the computation of baseball team triples consisting of a 
+    minimum number of players.
+    
+    Attributes: 
+        min_player_count (int): Minimum player count a given team triple must have in order to be returned.    
+    """
     def __init__(self, min_player_count):
+        """ Initializes the `TripleCounter` object.
+        
+        Args:
+            min_player_count (int): Cf. class docstring. 
+        """
         self.min_player_count = min_player_count
 
     def compute(self, df):
+        """ List baseball team triples with the required minimum number of players based on the data available in the
+        input DataFrame `df`.
+        
+        Args: 
+            df (pandas.DataFrame): DataFrame that gathers the raw data of all input baseball statistics files.
+
+        Returns: 
+            list[(frozenset, int)]: List of (team triple, player count) where each player count is greater or equal to
+            the value of the `min_player_count` attribute.
+        """
         df = df.drop_duplicates()
 
         df['team-id'] = np.where(df.league.isna(), df.team, df.team + '-' + df.league)
