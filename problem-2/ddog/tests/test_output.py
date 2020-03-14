c2902131654c07f30d547a1322ee9f96dee1959e
p-lnhrt <pierre.lienhart@gmail.com>
2020-03-14 18:04:01 +0100

    Add docstrings to ddog.output unit tests

--- problem-2/ddog/tests/test_output.py
+++ problem-2/ddog/tests/test_output.py
@@ -4,6 +4,11 @@ import ddog.output
 
 
 def test_preprocess_triples():
+    """
+    Given a list of (team triple, player count) tuples,
+    When I pass it to a function decorated with `ddog.output.preprocess_triples`,
+    Then the decorated function should be passed a list of formatted strings sorted in decreasing player count order.
+    """
     mock_sink = mock.Mock()
     func = mock.Mock()
     decorated_func = ddog.output.preprocess_triples(func)
@@ -18,9 +23,15 @@ def test_preprocess_triples():
 
 @mock.patch('logging.warning')
 def test_preprocess_triples_no_triples(mock_warning):
+    """
+    Given an empty list of (team triple, player count) tuples,
+    When I pass it to a function decorated with `ddog.output.preprocess_triples`,
+    Then the `logging.warning` functions should be called twice and the decorated function should not be called.
+    """
     mock_sink = mock.Mock()
     func = mock.Mock()
     decorated_func = ddog.output.preprocess_triples(func)
     triples = list()
     decorated_func(self=mock_sink, triples=triples)
     assert mock_warning.call_count == 2
+    func.assert_not_called()
